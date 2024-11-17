8d448bd2e9f10292066f3497fd92a8a624c440f9
Jaesung Kim <kisbi3@naver.com>
2024-11-18 00:31:37 +0900

    5.1 Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
 // 위젯 그 자체
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-// state: 위젯에 들어갈 데이터와 UI를 넣는 곳
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          headlineLarge: TextStyle(
+            color: Color(0xff232B55),
           ),
         ),
+        cardColor: const Color(0xffF4EDDB),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyLargeTitle(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  @override
-  // context: 모든 부모 요소들에 대한 정보
-  // 위젯 트리 안에서 위젯의 위치를 다룸.
-  // 위젯 트리에서 위젯의 위치를 제공하고, 이를 통해 상위 요소 데이터에 접근 가능.
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-          // ! : null이 아니라고 강력하게 확신한다는 의미.
-          // ? : null이 아니라면 사용하라는 의미.
-          fontSize: 30,
-          color: Theme.of(context).textTheme.titleLarge!.color),
+      home: const HomeScreen(),
     );
   }
 }
